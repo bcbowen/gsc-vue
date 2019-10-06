aab10097b808d388783439ff50b8622697bc1ccf
Ben Bowen <bcbowen@gmail.com>
2019-10-06 19:24:37 -0400

    Initial upload of files [WIP]
    Some preliminary setup, not ready for prime time

---
+++ src/home/WhatsNew.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <b>SAVE THE DATE!</b>
+    <ul>
+      <li>
+        Join us for
+        <span class="highlight">Happy Hour at our NEW LOCATION!</span> Every
+        <span class="highlight">Tuesday</span> when there isn't a General Meeting. The new location is:
+        <a href="https://www.bahamabreeze.com/home" target="_blank">Bahama Breeze, Oakland Park</a>, so come out and join us! We get started around 6:30 PM.
+      </li>
+      <li>
+        Meetings are held on the
+        <span class="highlight">third Tuesday</span> of each month. Click
+        <a href="http://www.gulfstreamsailingclub.org/meetings.php">here</a> for more info on the meetings.
+      </li>
+      <li>
+        <span class="highlight">Open Sail</span> every weekend at
+        <a
+          href="https://maps.google.com/maps?q=921+N.+Northlake+Drive,+Hollywood+FL&hl=en&sll=26.141112,-80.145567&sspn=0.208038,0.363579&t=h&hnear=921+N+Northlake+Dr,+Hollywood,+Broward,+Florida+33019&z=17&iwloc=A"
+          target="_blank"
+        >Sailor's Point</a> We have Optis, Sunfish &apos; 420s for your enjoyment. Saturday from 9:00 AM - 3:00 PM. Rentals still $5 for members.
+      </li>
+    </ul>
+
+    <p>
+      <image src="../images/boatusdiscount.jpg" />
+    </p>
+    <hr />
+    <p>
+      <span class="highlight">Special 20% discount code</span> on boat lettering for GSC members and friends. Just enter the following discount code at checkout:
+      <b>gulfstreamsailingclub</b>
+      <br />
+      <a target="_blank" href="https://www.letteringonthecheap.com/">
+        <image src="../images/lotc-logo.png" />
+      </a>
+    </p>
+    <hr />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WhatsNew',
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/sailorsPoint/SailorsPoint.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Sailor's Point PlaceHolder
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>

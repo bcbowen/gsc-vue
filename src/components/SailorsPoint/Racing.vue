df88753e692780fc9e165e693dfcb1d72bcea1c3
Ben Bowen <bcbowen@gmail.com>
2019-10-17 21:00:23 -0400

    [WIP] Added sailors point racing page

--- src/components/SailorsPoint/Racing.vue
+++ src/components/SailorsPoint/Racing.vue
@@ -1,15 +1,47 @@
 <template>
   <div>
+    <v-card>
+      <v-card-title>Gulfstream Sailing Club Racing Philosophy</v-card-title>
+      <v-card-text>
+        <br />We believe that racing furthers skill development for sailors young and old. Accordingly, Gulfstream Sailing Club holds an active schedule of dinghy and ocean capable racing. See below for the racing schedule for our Optimist (for children), Sunfish, and 420 racing. Racing is open to members and non-members alike.
+        <br />
+        <br />
 
+        <SunfishRacingSchedule />
+
+        <br/>
+        <h3>Commodores Cup</h3>
+        <b>Dec 7</b>
+
+        <hr />
+
+        <p>Skipper&apos;s meeting at 11:00 AM each day, races start shortly thereafter.</p>
+
+        <p>
+          <b>Race Fee:</b> $5.00
+        </p>
+
+        <p>
+          <b>Sunfish Rental Fee:</b> $5.00 for club members, $20.00 for non members
+        </p>
+
+        <p>
+          <b>Requirements:</b> Closed toed shoes (rubber soles) and life jackets must be worn when sailing.
+        </p>
+
+        <br />
+      </v-card-text>
+    </v-card>
   </div>
 </template>
 
 <script>
-  export default {
-    name: "Racing"
-  }
+import SunfishRacingSchedule from '../SailorsPoint/SunfishRacingSchedule'
+export default {
+  name: "Racing",
+  components: {SunfishRacingSchedule}
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
---
+++ src/components/SailorsPoint/SunfishRacingSchedule.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <h3>2019 Sunfish Racing Schedule</h3>
+    <hr />
+    <table>
+      <thead>
+        <th>Winter Series</th>
+        <th>Spring Series</th>
+        <th>Summer Series</th>
+        <th>Fall Series</th>
+      </thead>
+      <tr>
+        <td>{{formatDate(SunfishSchedule.Winter.Race1)}}</td>
+        <td>{{formatDate(SunfishSchedule.Spring.Race1)}}</td>
+        <td>{{formatDate(SunfishSchedule.Summer.Race1)}}</td>
+        <td>{{formatDate(SunfishSchedule.Fall.Race1)}}</td>
+      </tr>
+      <tr>
+        <td>{{formatDate(SunfishSchedule.Winter.Race2)}}</td>
+        <td>{{formatDate(SunfishSchedule.Spring.Race2)}}</td>
+        <td>{{formatDate(SunfishSchedule.Summer.Race2)}}</td>
+        <td>{{formatDate(SunfishSchedule.Fall.Race2)}}</td>
+      </tr>
+      <tr>
+        <td>{{formatDate(SunfishSchedule.Winter.Race3)}}</td>
+        <td>{{formatDate(SunfishSchedule.Spring.Race3)}}</td>
+        <td>{{formatDate(SunfishSchedule.Summer.Race3)}}</td>
+        <td>{{formatDate(SunfishSchedule.Fall.Race3)}}</td>
+      </tr>
+      <tr>
+        <td>{{formatDate(SunfishSchedule.Winter.Race4)}}</td>
+        <td>{{formatDate(SunfishSchedule.Spring.Race4)}}</td>
+        <td>{{formatDate(SunfishSchedule.Summer.Race4)}}</td>
+        <td>{{formatDate(SunfishSchedule.Fall.Race4)}}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+import SunfishData from "@/data/sunfishSchedule.json";
+export default {
+  data() {
+    return {
+      SunfishSchedule: SunfishData
+    };
+  },
+  methods: {
+    getMonth(val) {
+      const months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+      ];
+      return months[parseInt(val) - 1];
+    },
+    formatDate(raceDate) {
+      return `${this.getMonth(raceDate.Month)} ${raceDate.Day}`;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>

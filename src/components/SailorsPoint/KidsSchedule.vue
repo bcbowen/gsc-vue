95e114e4d50ddcc9f78f0bee5c6e114dfa0057e4
Ben Bowen <bcbowen@gmail.com>
2019-12-29 00:10:46 -0500

    Cleanup commented code

--- src/components/SailorsPoint/KidsSchedule.vue
+++ src/components/SailorsPoint/KidsSchedule.vue
@@ -1,7 +1,5 @@
 <template>
   <div>
-    <h3></h3>
-    
     <v-card max-height="300px" class="overflow-y-auto">
       <v-card-title>{{ scheduleData.Year }} Learn-to-Sail Schedule (kids ages 8 to 14 - level 1)</v-card-title>
       <v-expansion-panels accordion>
@@ -23,28 +21,6 @@
       </v-expansion-panels>
     </v-card>
 
-    <!--
-    <h4>Sessions:</h4>
-    <v-form>
-      <v-select
-        :items="scheduleData.Sessions"
-        item-text="Session"
-        item-value="Session"
-        label="Session Number"
-        @change="selectSession"
-        v-model="selectedSession"
-      ></v-select>
-      <v-list>
-        <v-list-item>Registration: {{ selectedSession.Registration }}</v-list-item>
-        <v-list-item>Class 1: {{ selectedSession.Classes[0] }}</v-list-item>
-        <v-list-item>Class 2: {{ selectedSession.Classes[1] }}</v-list-item>
-        <v-list-item>Class 3: {{ selectedSession.Classes[2] }}</v-list-item>
-        <v-list-item>Class 4: {{ selectedSession.Classes[3] }}</v-list-item>
-        <v-list-item>Class 5: {{ selectedSession.Classes[4] }}</v-list-item>
-        <v-list-item>Class 6: {{ selectedSession.Classes[5] }}</v-list-item>
-      </v-list>
-    </v-form>
-    -->
   </div>
 </template>
 
@@ -52,26 +28,12 @@
 export default {
   data () {
     return {
-      //selectedSession: this.scheduleData.Sessions[0]
     }
   },
   props: {
     scheduleData: {
       type: Object
     }
-  },
-  methods: {
-    /*selectSession: function (id) {
-      let index = 0
-      for (var i = 0; i < this.scheduleData.Sessions.length; i++) {
-        if (this.scheduleData.Sessions[i].Session === id) {
-          index = i
-          break
-        }
-      }
-      this.selectedSession = this.scheduleData.Sessions[index]
-    }
-    */
   }
 }
 </script>

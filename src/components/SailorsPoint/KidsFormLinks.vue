da15d41be1ac3346f2c97c731483f0a40253a404
Ben Bowen <bbowen@bhgpdi.net>
2021-03-31 22:45:19 -0400

    Updates to kids registration info

--- src/components/SailorsPoint/KidsFormLinks.vue
+++ src/components/SailorsPoint/KidsFormLinks.vue
@@ -3,24 +3,51 @@
     <h3>The following are required to register for classes:</h3>
     <ul>
       <li>
-        Sailors Point Registration Checklist (Can be downloaded
-        <a href="http://www.gulfstreamsailingclub.org/docs/SailorsPointForms/2021_Kids_Registration_Checklist.pdf" target="_new">here</a>)
+        <strong>Sailors Point Registration Checklist - includes all forms</strong> (Can be downloaded
+        <a href="http://www.gulfstreamsailingclub.org/docs/SailorsPointForms/SailorsPointKidsRegistrationChecklist.pdf" target="_new">here</a>)
       </li>
       <li>
-        Registration Form (Can be downloaded
+         <strong>Registration Form </strong> (Can be downloaded
         <a href="http://www.gulfstreamsailingclub.org/docs/SailorsPointForms/2021_Kids_Registration_Form.pdf" target="_new">here</a>)
       </li>
-      <li>Check for $180 ($125 for club members or Hollywood residents with proof of residency)</li>
+      <li><strong>Payments</strong> All checks should be written out to: Gulfstream Sailing Club. We do not accept digital payments.
+         <ul>
+            <li>Sunday Sailing Classes (6 sessions)
+              <ul>
+                <li>
+                  Hollywood Residents/Gulfstream Club Members: Check for $125 (Proof of residency required)
+                </li>
+                <li>
+                  Non-Residents or Club Members: Check for $180
+                </li>
+              </ul>
+            </li>
+            <li>Summer Camp (1 week)
+              <ul>
+                <li>
+                  Hollywood Residents/Gulfstream Club Members: Check for $150 for the first child and $125 for each sibling  (Proof of residency required)
+                </li>
+                <li>
+                  Non-Residents or Club Members: Check for $200 for first child and $175 for each sibling
+                </li>
+              </ul>
+            </li>
+          </ul>
       <li>
-        Safety Deposit Form (Can be downloaded
+         <strong>Safety Deposit Form</strong> (Can be downloaded
         <a href="http://www.gulfstreamsailingclub.org/docs/SailorsPointForms/2021_Kids_Safety_Deposit_Form.pdf" target="_new">here</a>)
       </li>
-      <li>Check for $50 damage/security (only cashed if failure to meet the items on the security waiver)</li>
-      <li>Front/Back copy of healthcare card</li>
+      <li> <strong>Check for $50 damage/security </strong> (only cashed if failure to meet the items on the security waiver, must be separate check)</li>
+      <li> <strong>Copy of healthcare card </strong> (Front/Back )</li>
       <li>
-        COVID-19 Waiver (Can be downloaded
+         <strong>COVID-19 Waiver</strong> (Can be downloaded
         <a href="http://www.gulfstreamsailingclub.org/docs/SailorsPointForms/COVID-19-Waiver-doc.pdf" target="_new">here</a>)
       </li>
     </ul>
+    <br />
+    <p>
+    All checks should be written out to: Gulfstream Sailing Club.<br />
+    We do not accept digital payments.
+   </p>
   </div>
 </template>

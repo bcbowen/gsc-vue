2fedb26ff32ec32da51b3782730a83bb77c7914a
Ben Bowen <bbowen@bhgpdi.net>
2021-06-02 18:07:25 -0400

    Remove covid updates and add may minutes

--- src/components/HappyHour/HappyHour.vue
+++ src/components/HappyHour/HappyHour.vue
@@ -3,9 +3,11 @@
     <v-card>
       <v-card-title>Happy Hour</v-card-title>
       <v-card-text>
+        <strong>
         Happy Hours happen every week! We meet every
         Tuesday at 6:30, except when there is a General Meeting.
-        Unfortunately, due to COVID-19, our happy hours have moved to Zoom until we have the all clear.
+        Currently we are meeting at the Quarterdeck,  near 17th Street in Fort Lauderdale. This may change so stay tuned.
+        </strong>
       </v-card-text>
     </v-card>
   </div>
--- src/components/MeetingInfo.vue
+++ src/components/MeetingInfo.vue
@@ -4,21 +4,13 @@
       <v-card>
         <v-card-title>Meetings</v-card-title>
         <v-card-text>
-          <h3 class="redText">COVID-19 Update:</h3>
           <p>
             <strong>
-              Please note that we will not be meeting at our usual meeting spot until the shelter in place order has been lifted. For the time
-              being, we are meeting on zoom. Everyone on our mailing list is sent the link prior to the meeting.
+              Meetings are held on the third Tuesday of each month. <br />
+              Due to the recent pandemic, we have lost our previous regular meeting spot. Please check our Facebook site or email, or contact a 
+              current member for updates.
             </strong>
-          </p>Meetings are held on the third Tuesday of each month. The location for our meetings is the VFW Post 1966 on SR 84
-          (near West Marine and BOW). The address for our meetings is:
-          <p>
-            350 SW 25th St
-            <br />Fort Lauderdale, FL
-          </p>Show up a little early to
-          order food and drinks, and meetings start promptly at 8pm.
-          <br />We try to keep the meetings to about 1 hour in length, with varying degrees of success.
-          <br />Meetings are open to all, so please stop by!
+          </p>
         </v-card-text>
       </v-card>
     </v-container>
